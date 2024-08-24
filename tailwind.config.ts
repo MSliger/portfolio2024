44e8ad95ab95592923d38c9a5735f93a8dc15ff8
MSliger <matthewsliger@gmail.com>
2024-08-24 13:18:55 -0400

    tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,10 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  content: [],
   theme: {
     extend: {},
   },
   plugins: [],
-} satisfies Config;
+} satisfies Config
+
